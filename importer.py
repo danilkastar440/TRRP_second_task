0ea91ee1f65e70a463a415d4ed3259f1db7fe324
Danilkastar440 <pizza.yeaaah@gmail.com>
2020-06-24 23:28:10 +0500

    Import data from sqlite3

--- importer.py
+++ importer.py
@@ -8,15 +8,13 @@ from kafka import KafkaConsumer
 def by_kafka():
     consumer = KafkaConsumer('TutorialTopic', bootstrap_servers='10.8.0.1:9092')
     
-    #processing for db
-    normalize.prepare_db()
-    print("Database is ready, waiting for messages from kafka on 10.8.0.1")
-    
     for msg in consumer:
-        item = []
-        item.append(tuple(msg.value.decode("utf-8").split(',')))
-        normalize.write_data_mysql(item)
-        #print(item)
+        items = []
+        item = msg.value.decode("utf-8")[1:-1].replace("'",'').replace(', ',',').split(',')
+        #items should be list of tuples or list of lists
+        items.append(item)
+        #print(type(msg.value.decode("utf-8")))
+        normalize.write_data_mysql(items)
 
 
 
@@ -25,7 +23,6 @@ def by_socket():
     #processing for db
     normalize.prepare_db()
     print("Database is ready, waiting for connections on ws://10.8.0.1:9991")
-    
     
     start_server = websockets.serve(get_information, "10.8.0.1", 9991)
     asyncio.get_event_loop().run_until_complete(start_server)
@@ -36,23 +33,29 @@ def by_socket():
 async def get_information(websocket, path):
     try:
         while True:
+            items = []
             string = await websocket.recv()
-            item = []
-            #print(f'item: {item}')
-            item.append(tuple(string.rstrip().split(',')))
-            normalize.write_data_mysql(item)
+            item = string[1:-1].replace("'",'').replace(', ',',').split(',')
+            #items should be list of tuples or list of lists
+            items.append(item)
+            #print(items)
+            normalize.write_data_mysql(items)
     except:
         pass
 
 if __name__=="__main__":
-	parser = argparse.ArgumentParser(description='Import data to mysql')
-	parser.add_argument('--kafka', help='Import by kafka', action="store_true")
-	parser.add_argument("--socket", help="Import by socket", action="store_true")
-	args = parser.parse_args()
-	
-	if args.kafka:
-		by_kafka()
-	elif args.socket:
-		by_socket()
-	else:
-		print("Add some startup options")
+    parser = argparse.ArgumentParser(description='Import data to mysql')
+    parser.add_argument('--kafka', help='Import by kafka', action="store_true")
+    parser.add_argument("--socket", help="Import by socket", action="store_true")
+    args = parser.parse_args()
+    
+    #processing for db
+    normalize.prepare_db()
+    print("Database is ready, waiting for messages from kafka on 10.8.0.1")
+    
+    if args.kafka:
+    	by_kafka()
+    elif args.socket:
+    	by_socket()
+    else:
+    	print("Add some startup options")
--- producer.py
+++ producer.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env  python3
 import asyncio
+import sqlite3
 import argparse
+import normalize
 import websockets
 from kafka import KafkaProducer
 
 
-f = open('baza.txt','r')
 
-def by_kafka():
+def by_kafka(data):
     producer = KafkaProducer(bootstrap_servers='10.8.0.1:9092', api_version = (1,0,0))
-    for item in f:
-        #print(bytes(item[:-1], 'utf-8'))
-        producer.send('TutorialTopic', bytes(item[:-1],'utf-8'))
+    for item in data:
+        print(item)
+        producer.send('TutorialTopic', bytes(str(item),'utf-8'))
     producer.close()
-    f.close()
 
-def by_socket():
+def by_socket(data):
     async def send_information():
         uri = "ws://10.8.0.1:9991"
         async with websockets.connect(uri) as websocket:
-            for item in f:
-                print(f'sent: {item.rstrip()}')
-                await websocket.send(item.rstrip())
+            for item in data:
+                print(f'sent: {item}')
+                #type(item) == tuple
+                await websocket.send(str(item))
     asyncio.get_event_loop().run_until_complete(send_information())
 
 if __name__=="__main__":
@@ -29,10 +30,12 @@ if __name__=="__main__":
         parser.add_argument('--kafka', help='Send by kafka', action="store_true")
         parser.add_argument("--socket", help="Send by socket", action="store_true")
         args = parser.parse_args()
-
+        
+        #list of tuples
+        data = normalize.read_data_sqlite3("baza.db")
         if args.kafka:
-                by_kafka()
+                by_kafka(data)
         elif args.socket:
-                by_socket()
+                by_socket(data)
         else:
                 print("Add some startup options")
